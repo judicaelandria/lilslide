715433bb70ae3611ba162a53b01690813c5c1417
judicael_ai <judicael@smartpredict.ai>
2022-01-27 21:48:11 +0300

    Feature(page): create a new page

--- app/icons/ChevronLeftIcon.tsx
+++ app/icons/ChevronLeftIcon.tsx
@@ -1,11 +1,11 @@
 import { IIconProps } from "./type";
 
-const DesktopIcon = ({ color = "#fff" }: IIconProps) => {
+const ChevronLeftIcon = ({ color = "#fff" }: IIconProps) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      className="h-4 w-4"
-      fill={color}
+      className="h-6 w-6"
+      fill="none"
       viewBox="0 0 24 24"
       stroke={color}
     >
@@ -13,10 +13,10 @@ const DesktopIcon = ({ color = "#fff" }: IIconProps) => {
         strokeLinecap="round"
         strokeLinejoin="round"
         strokeWidth={2}
-        d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+        d="M15 19l-7-7 7-7"
       />
     </svg>
   );
 };
 
-export default DesktopIcon;
+export default ChevronLeftIcon;
